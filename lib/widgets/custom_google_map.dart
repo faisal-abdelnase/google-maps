7cec51fb9167eff633bcd1f0d4aac92a07e5f65a
faisal <faisalabdalnasser275@gmail.com>
2025-02-04 18:48:08 +0200

    add holes inside polygon

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -18,7 +18,6 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition _initialCameraPosition;
 
   Set<Marker> _markers = {};
-  Set<Polyline> _polylines = {};
   Set<Polygon> _polygons = {};
 
   @override
@@ -26,23 +25,12 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     _initialCameraPosition = const CameraPosition(
       // this is initial latitude and longitude of the camera
       target: LatLng(29.86398403496827, 31.302744328217248),
-
-      // this is the zoom level of the camera
-      /**
-       * world view 0 -> 3
-       * country view 4 -> 6
-       * city view 10 -> 12
-       * street view 13 -> 17
-       * building view 18 -> 20
-       */
       zoom: 12,
 
       );
 
 
       initMarkers();
-
-      initPolyLines();
       initPolygons();
 
       
@@ -60,16 +48,11 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     return 
     
     GoogleMap(
-    // this is the initial camera position
-    //this is position of the camera when the map is loaded
-    // no update to initialCameraPosition
           
     initialCameraPosition: _initialCameraPosition,
     
     mapType: MapType.normal,
-
     markers: _markers,
-    // polylines: _polylines,
     polygons: _polygons,
 
     // zoomControlsEnabled: false,
@@ -132,44 +115,6 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
 
 
   
-  void initPolyLines() {
-    Polyline polyline1 =  Polyline(
-      polylineId: const PolylineId('1'),
-      color: Colors.red,
-      width: 5,
-      startCap: Cap.roundCap,
-      zIndex: 10,
-      patterns: [
-        PatternItem.dash(20),
-        PatternItem.gap(10),
-      ],
-      geodesic: true,
-      points: const [
-        LatLng(29.850049845029098, 31.340459028109382),
-        LatLng(29.843587027862636, 31.342500437337772),
-        LatLng(29.84511510781167, 31.330146979891676),
-      ],
-    );
-
-
-
-    Polyline polyline2 = const Polyline(
-      polylineId: PolylineId('2'),
-      color: Colors.black,
-      width: 5,
-      startCap: Cap.roundCap,
-      zIndex: 5,
-      points: [
-        LatLng(29.854194245099112, 31.335932166539973),
-        LatLng(29.83826211060132, 31.33349873964596),
-        
-      ],
-    );
-
-    _polylines.add(polyline1);
-    _polylines.add(polyline2);
-
-  }
 
 
 
@@ -177,6 +122,10 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   
   void initPolygons() {
     Polygon polygon1 = Polygon(
+      holes: const [[ 
+        LatLng(29.845457922458667, 31.336319168431356),
+        LatLng(29.845431601490677, 31.335226722121526),
+        LatLng(29.844931501049505, 31.337381268932916),]],
       polygonId: const PolygonId('1'),
       fillColor: Colors.red.withOpacity(0.5),
       strokeColor: Colors.orange,
