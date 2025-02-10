3af5f5fbbe5e93c524b83b69505eff39fcd99860
faisal <faisalabdalnasser275@gmail.com>
2025-02-11 00:37:35 +0200

    Refactor code

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -1,7 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:google_maps/utils/location_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:location_platform_interface/location_platform_interface.dart';
 
 class CustomGoogleMap extends StatefulWidget {
   const CustomGoogleMap({super.key});
@@ -13,18 +14,18 @@ class CustomGoogleMap extends StatefulWidget {
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
 
   late CameraPosition _initialCameraPosition;
-  late Location location;
+  late LocationService locationService;
 
   @override
   void initState() {
     _initialCameraPosition = const CameraPosition(
       // this is initial latitude and longitude of the camera
       target: LatLng(29.86398403496827, 31.302744328217248),
-      zoom: 15,
+      zoom: 1,
 
       );
 
-    location = Location();
+    locationService = LocationService();
 
       updateMyLocation();
 
@@ -36,6 +37,8 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   
 
 GoogleMapController? _googleMapController;
+
+bool isFirstCall = true;
 
 Set<Marker> _markers = {};
 
@@ -77,77 +80,64 @@ Set<Marker> _markers = {};
 
 
   
-  Future<void> checkAndRequestLocationService() async {
-    var isServiceEnable = await location.serviceEnabled();
 
-    if(!isServiceEnable){
-      isServiceEnable = await location.requestService();
-      if(!isServiceEnable){
-        return;
-      }
-    }
-
-  }
-
-
-
-
-  
-  Future<bool> checkAndRequestLocationPermission() async {
-    var permissionStatus = await location.hasPermission();
-
-    if(permissionStatus == PermissionStatus.denied){
-      permissionStatus = await location.requestPermission();
-      if(permissionStatus != PermissionStatus.granted){
-        return false;
-    }
-    }
-
-    if(permissionStatus == PermissionStatus.deniedForever){
-      return false;
-    }
-
-    return true;
-  }
-
-
-
-void getLocationData(){
-  
-  location.changeSettings(
-    distanceFilter: 2,
-  );
-  location.onLocationChanged.listen((locationData){
-    _googleMapController?.animateCamera(
-      CameraUpdate.newLatLng(
-        LatLng(locationData.latitude!, locationData.longitude!)),
-    );
-
-    _markers.add(
-      Marker(
-        markerId: const MarkerId('my_location'),
-        position: LatLng(locationData.latitude!, locationData.longitude!),
-        infoWindow: const InfoWindow(
-          title: 'My Location',
-        ),
-      ),
-    );
-
-    setState(() {
-      
-    });
-  });
-}
 
 
 
   void updateMyLocation() async {
-
-    await checkAndRequestLocationService();
-    var hasPermission = await checkAndRequestLocationPermission();
+    await locationService.checkAndRequestLocationService();
+    var hasPermission = await locationService.checkAndRequestLocationPermission();
     if(hasPermission){
-      getLocationData();
+      locationService.getRealTimeLocationData((locationData){
+
+        setMylocationMarker(locationData);
+
+        updateMyCamera(locationData);
+
+        
+      });
     }else{}
+  }
+
+
+
+  void updateMyCamera(LocationData locationData) {
+    if (isFirstCall) {
+      CameraPosition cameraPosition = CameraPosition(
+        target: LatLng(locationData.latitude!, locationData.longitude!),
+        zoom: 17,
+      );
+      _googleMapController?.animateCamera(
+        CameraUpdate.newCameraPosition(cameraPosition),
+      );
+
+      isFirstCall = false;
+
+    }
+    else{
+      _googleMapController?.animateCamera(
+        CameraUpdate.newLatLng(
+          LatLng(locationData.latitude!, locationData.longitude!)),
+      );
+    }
+  }
+
+  
+  
+  void setMylocationMarker(LocationData locationData) {
+      _markers.add(
+          Marker(
+            markerId: const MarkerId('my_location'),
+            position: LatLng(locationData.latitude!, locationData.longitude!),
+            infoWindow: const InfoWindow(
+              title: 'My Location',
+            ),
+          ),
+        );
+    
+    setState(() {
+      
+    });
   }
 
 
